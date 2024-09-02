d8995483f915f8d3edac7158947d8d5e2c2fbd0e
ricsimarta <ricsimarta@gmail.com>
2024-09-02 19:46:57 +0200

    added static directory. and fetch data from backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello codecool!')
+  res.sendFile(path.join(__dirname, '/../frontend/index.html'))
+  res.send("hello world")
+})
+
+app.use('/public', express.static(path.join(__dirname, '/../frontend/public')))
+
+app.get('/data', (req, res) => {
+  res.json({ name: "kismacska" })
 })
 
 app.listen(port, () => {
