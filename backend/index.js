18b64d6f9548fd1643e901a5cbfc62f53689fb2d
ricsimarta <ricsimarta@gmail.com>
2024-09-04 19:02:58 +0200

    added data endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,49 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/../frontend/index.html'))
-  res.send("hello world")
 })
 
 app.use('/public', express.static(path.join(__dirname, '/../frontend/public')))
 
 app.get('/data', (req, res) => {
-  res.json({ name: "kismacska" })
+  fs.readFile(`${__dirname}/data.json`, (err, data) => {
+    if (err) {
+      console.log("error at reading the file", err)
+      res.json("error at reading the file")
+    } else {
+      const jsonData = JSON.parse(data)
+      const result = jsonData.map(obj => obj.id)
+      res.json(result)
+    }
+  })
+})
+
+app.get('/data/:id', (req, res) => {
+  console.log(req.params)
+  const searchId = Number(req.params.id)
+  // itt
+
+  fs.readFile(`${__dirname}/data.json`, (err, data) => {
+    if (err) {
+      console.log("error at reading the file", err)
+      res.status(500).json("error at reading the file")
+    } else {
+      const jsonData = JSON.parse(data)
+      const result = jsonData.find(obj => obj.id === searchId)
+      
+      if (result === undefined) {
+        res.status(404).json(`no user found with id ${searchId}`)
+      } else {
+        res.status(200).json(result)
+      }
+    }
+  })
 })
 
 app.listen(port, () => {
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -3,3 +3,11 @@ console.log("loaded")
 fetch('/data')
   .then(res => res.json())
   .then(data => console.log(data))
+
+fetch('/data/kismacska')
+  .then(res => res.json())
+  .then(data => console.log(data))
+
+fetch('/data/120')
+  .then(res => res.json())
+  .then(data => console.log(data))
